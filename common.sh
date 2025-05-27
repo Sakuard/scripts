a0b8ecfca350627621880764f55373f75bef7eab
技術處-平台部-監控管理組Matt_Huang <matt_huang@mail.chungyo.net>
2025-05-27 15:43:36 +0800

    feat: optimize

--- common.sh
+++ common.sh
@@ -25,3 +25,23 @@ package_check() {
         esac
     fi
 }
+tap_package_check() {
+    local cmd=$1
+    local tap=$2
+    local pkg=$3
+    
+    if ! command -v ${cmd} 1>/dev/null; then
+        read -r -e -p "本腳本需要安裝 ${cmd}，請確認是否安裝並繼續執行？(Y/N)：" continue
+        case $continue in
+            Y | y)
+                brew tap ${tap}
+                brew install ${pkg} ;;
+            N | n)
+                echo -e "${RED}未安裝 ${cmd}，退出腳本${WHITE}"
+                exit 1 ;;
+            *)
+                echo -e "${RED}無效參數 ($REPLY)，請重新輸入${WHITE}"
+                exit 1 ;;
+        esac
+    fi
+}
